b47550508d3e30aea0ffba680932d0dbc1f5c6f8
Daewon Choe <daewon.choe@target.com>
2017-12-02 01:22:26 -0600

    more updates

--- football-deep-predictor/src/main/groovy/deep/learning/FootballPredictor.groovy
+++ football-deep-predictor/src/main/groovy/deep/learning/FootballPredictor.groovy
@@ -21,10 +21,30 @@ class FootballPredictor {
         def new_orleans = [name: 'new_orleans', off_passing: 2940, off_passing_game: 267, off_rushing: 1563, off_rushing_game: 142,
         def_passing: 2440, def_passing_game: 222, def_rushing: 1242, def_rushing_game: 113]
 
+        def tampa_bay = [name: 'tampa_bay', off_passing: 2909, off_passing_game: 264, off_rushing: 892, off_rushing_game: 81,
+        def_passing: 3131, def_passing_game: 285, def_rushing: 1220, def_rushing_game: 111]
+
+        def la_chargers = [name: 'la_chargers', off_passing: 2903, off_passing_game: 264, off_rushing: 1025, off_rushing_game: 93,
+        def_passing: 2266, def_passing_game: 206, def_rushing: 1468, def_rushing_game: 1134]
+
+        def pittsburgh = [name: 'pittsburgh', off_passing: 2892, off_passing_game: 263, off_rushing: 1145, off_rushing_game: 104,
+        def_passing: 2127, def_passing_game: 193, def_rushing: 1056, def_rushing_game: 96]
+
+        def cleveland = [name: 'cleveland', off_passing: 2231, off_passing_game: 203, off_rushing: 1181, off_rushing_game: 107,
+        def_passing: 2429, def_passing_game: 221, def_rushing: 1069, def_rushing_game: 97]
+
+        def buffalo = [name: 'buffalo', off_passing: 2008, off_passing_game: 183, off_rushing: 1281, off_rushing_game: 117,
+        def_passing: 2650, def_passing_game: 241, def_rushing: 1254, def_rushing_game: 114]
+
+        def baltimore = [name: 'baltimore', off_passing: 1807, off_passing_game: 164, off_rushing: 1285, off_rushing_game: 117,
+        def_passing: 2089, def_passing_game: 190, def_rushing: 1274, def_rushing_game: 116]
+
+
+
         calc(atlanta, minnesota)
-        calc(minnesota, detroit)
-        calc(washington, minnesota)
-        calc(minnesota, new_orleans)
+        calc(la_chargers, cleveland)
+        calc(new_england, buffalo)
+        calc(baltimore, detroit)
     }
 
     private static void calc(Map home, Map visitor) {
@@ -41,7 +61,7 @@ class FootballPredictor {
         }
         else {
             def passing_diff = visitor['def_passing_game'] - home['off_passing_game']
-            def total_passing = (visitor['def_passing_game'] + (passing_diff / 5))
+            def total_passing = (visitor['def_passing_game'] - (passing_diff / 5))
             home_tds = total_passing / 100
             println('home passing: ' + home['name'] + ' ' + total_passing)
         }
@@ -55,7 +75,7 @@ class FootballPredictor {
         }
         else {
             def rushing_diff = visitor['def_rushing_game'] - home['off_rushing_game']
-            def total_rushing = (visitor['def_rushing_game'] + (rushing_diff / 5))
+            def total_rushing = (visitor['def_rushing_game'] - (rushing_diff / 5))
             home_tds = home_tds + (total_rushing / 100)
             println('home passing: ' + home['name'] + ' ' + total_rushing)
         }
@@ -71,7 +91,7 @@ class FootballPredictor {
         }
         else {
             def passing_diff = visitor['off_passing_game'] - home['def_passing_game']
-            def total_passing = (visitor['off_passing_game'] + (passing_diff / 5))
+            def total_passing = (visitor['off_passing_game'] - (passing_diff / 5))
             visitor_tds = total_passing / 100
             println('visitor passing: ' + visitor['name'] + ' ' + total_passing)
         }
@@ -85,9 +105,9 @@ class FootballPredictor {
         }
         else {
             def rushing_diff = visitor['off_rushing_game'] - home['def_rushing_game']
-            def total_rushing = (visitor['off_rushing_game'] + (rushing_diff / 5))
+            def total_rushing = (visitor['off_rushing_game'] - (rushing_diff / 5))
             visitor_tds = visitor_tds + (total_rushing / 100)
-            println('visitor passing: ' + visitor['name'] + ' ' + total_rushing)
+            println('visitor rushing: ' + visitor['name'] + ' ' + total_rushing)
         }
 
         def home_score = home_tds * 7
