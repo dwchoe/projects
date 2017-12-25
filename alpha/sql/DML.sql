32a54c430f4d88f70de83e8f9787637264f212d6
dwchoe <dwchoe@users.noreply.github.com>
2017-12-25 00:36:47 -0600

    Update DML.sql

--- alpha/sql/DML.sql
+++ alpha/sql/DML.sql
@@ -11,3 +11,17 @@ insert into historic_return(category, year, return) values ('Gold','1971-12-31',
 insert into historic_return(category, year, return) values ('Gold','1972-12-31',43.14);
 insert into historic_return(category, year, return) values ('Gold','1973-12-31',66.79);
 insert into historic_return(category, year, return) values ('Gold','1974-12-31',72.59);
+
+5YR Treasury - https://fred.stlouisfed.org/series/GS5
+insert into historic_return(category, year, return) values ('5YR Treasury','1970-12-31',5.94);
+insert into historic_return(category, year, return) values ('5YR Treasury','1971-12-31',5.64);
+insert into historic_return(category, year, return) values ('5YR Treasury','1972-12-31',6.26);
+insert into historic_return(category, year, return) values ('5YR Treasury','1973-12-31',6.74);
+insert into historic_return(category, year, return) values ('5YR Treasury','1974-12-31',7.10);
+
+10YR Treasury - https://fred.stlouisfed.org/series/GS5
+insert into historic_return(category, year, return) values ('10YR Treasury','1970-12-31',6.28);
+insert into historic_return(category, year, return) values ('10YR Treasury','1971-12-31',6.00);
+insert into historic_return(category, year, return) values ('10YR Treasury','1972-12-31',5.96);
+insert into historic_return(category, year, return) values ('10YR Treasury','1973-12-31',7.29);
+insert into historic_return(category, year, return) values ('10YR Treasury','1974-12-31',7.98);
