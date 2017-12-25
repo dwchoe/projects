948312c1d6d1712c080ddf714cdfa51d1128a3f0
dwchoe <dwchoe@users.noreply.github.com>
2017-12-25 00:14:45 -0600

    Update DML.sql

--- alpha/sql/DML.sql
+++ alpha/sql/DML.sql
@@ -7,7 +7,7 @@ insert into historic_return(category, year, return) values ('SP500','1974-12-31'
 
 Gold
 insert into historic_return(category, year, return) values ('Gold','1970-12-31',-5.12);
-insert into historic_return(category, year, return) values ('SP500','1971-12-31',14.65);
-insert into historic_return(category, year, return) values ('SP500','1972-12-31',43.14);
-insert into historic_return(category, year, return) values ('SP500','1973-12-31',66.79);
-insert into historic_return(category, year, return) values ('SP500','1974-12-31',72.59);
+insert into historic_return(category, year, return) values ('Gold','1971-12-31',14.65);
+insert into historic_return(category, year, return) values ('Gold','1972-12-31',43.14);
+insert into historic_return(category, year, return) values ('Gold','1973-12-31',66.79);
+insert into historic_return(category, year, return) values ('Gold','1974-12-31',72.59);
