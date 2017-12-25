cde14d2cf1015e0492a39b6e29e386b2f4c560fb
dwchoe <dwchoe@users.noreply.github.com>
2017-12-25 00:04:45 -0600

    Create DML.sql

---
+++ alpha/sql/DML.sql
@@ -0,0 +1,7 @@
+SP500
+insert into historic_return(category, year, return) values ('SP500','1970-12-31',0.1);
+insert into historic_return(category, year, return) values ('SP500','1971-12-31',10.79);
+insert into historic_return(category, year, return) values ('SP500','1972-12-31',15.63);
+insert into historic_return(category, year, return) values ('SP500','1973-12-31',-17.37);
+insert into historic_return(category, year, return) values ('SP500','1974-12-31',-29.72);
+
